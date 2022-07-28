4011968aa089bdbd76234a35f326cad5da196fc4
jjjy879 <jjjy879@naver.com>
2022-07-28 17:32:38 +0900

    .

---
+++ src/com/coocha/study/ch02/ByteExample.java
@@ -0,0 +1,25 @@
+package com.coocha.study.ch02;
+
+public class ByteExample {
+
+	public static void main(String[] args) {
+		
+		byte var1 = -128;
+		byte var2 = -30;
+		byte var3 = 0;
+		byte var4 = 30;
+		byte var5 = 127;
+//		byte var6 = 128;
+		short var6 = 128;
+		
+//byte 데이터범위 : -128~127까지임으로 범위 밖의 숫자를 입력하면 error
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+	}
+
+}
