1360383a6a1e8468812956071d1663759b00d027
jjjy879 <jjjy879@naver.com>
2022-08-03 17:25:17 +0900

    .

--- src/com/coocha/study/ch03/ArithmeticOperatorExample.java
+++ src/com/coocha/study/ch03/ArithmeticOperatorExample.java
@@ -24,7 +24,52 @@ public class ArithmeticOperatorExample {
 		
 		double result7 = (double) v1 / v2;
 		System.out.println("result7 : " +result7);
+		
+		System.out.println("--------------------");
+		
+		double a = (double)(1*3+2)/3;
+		double b = (double)5/8;
+		double c = (double)(9*5+4)/5;
+		
+		int d = 5;
+		double e = 3.25;
+		double f = (double)(1*6+5)/6;
+		double g = (double)12/17;
+		
+		double num1 = (a-b)*c;
+		double num2 = d-(e-f)*g;
+		
+		System.out.println("num1 : " +num1);
+		System.out.println("num2 : " +(int)num2);
+		
+		System.out.println("--------------------");
+	
 
+/*		sum(8,7,5);
+		sum(1,9,5);
+		sum(5,6,6);
+		sum(4,7,1);
+		sum(7,5,5);
+		
+	}
+
+	 public static void sum(int a, int b, int c) {
+	  
+	 System.out.println(a + " + " + b + " + "+ c + " = " + (a+b+c));
+	 
+	 } System.out.println("--------------------");
+*/	
+	
+	avg("홍길동",60,70,85,90,45);
+	avg("박말순",50,75,70,60,70);
+	avg("고장난",55,60,64,58,90);
+	avg("도룡뇽",80,70,63,88,78);
+	}
+	
+	public static void avg(String name, int english, int math, int physics, int music, int athletic) {
+		
+		
+		System.out.println(name + "의 평균은 " + (double)(english+math+physics+music+athletic)/5 + "점입니다.");
 	}
 
 }
---
+++ src/com/coocha/study/ch03/CompareOperatorExample.java
@@ -0,0 +1,26 @@
+package com.coocha.study.ch03;
+
+public class CompareOperatorExample {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		boolean result1 = (num1 == num2);
+		boolean result2 = (num1 != num2);
+		boolean result3 = (num1 < num2);
+		boolean result4 = (num1 <= num2);
+		boolean result5 = (num1 > num2);
+		boolean result6 = (num1 >= num2);
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(result4);
+		System.out.println(result5);
+		System.out.println(result6);
+		
+	}
+
+}
