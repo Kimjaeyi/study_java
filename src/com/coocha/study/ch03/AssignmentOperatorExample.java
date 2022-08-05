8d862a32594c091b394e87a3d0f9a2c274dfefde
jjjy879 <jjjy879@naver.com>
2022-08-05 11:24:09 +0900

    .

---
+++ src/com/coocha/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,28 @@
+package com.coocha.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+
+		int result = 0;
+		int result2 = 3;
+		int result3 = 5;
+		
+//		+=
+		result += 10;
+//		result = result + 10;
+		System.out.println("result : " + result);
+		
+//		-=
+		result2 -= 10;
+//		result2 = result2 - 10;
+		System.out.println("result2 : " + result2);
+		
+//		*=
+		result3 *= 10;
+//		result3 = result3 * 10;
+		System.out.println("result3 : " + result3);
+		
+	}
+
+}
