3f4567794c680e8fa23bb31f284372e230b827d7
jjjy879 <jjjy879@naver.com>
2022-08-19 17:20:11 +0900

    .

--- src/com/coocha/study/ch05/ArrayInArrayExample.java
+++ src/com/coocha/study/ch05/ArrayInArrayExample.java
@@ -9,11 +9,11 @@ public class ArrayInArrayExample {
 		System.out.println("scores[1][1]: " + scores[1][1]);
 		System.out.println("--------------------");
 		
-		// 1. 개인의 평균 2. 과목별 평균
-		
 		String name[] = {"홍길동", "김재이", "정우성", "이정재", "장원영"};
 		String subject[] = {"국어", "수학", "영어"};
 	
+		
+//		1. 개인의 평균
 /*		
 		for(int i=0; i<scores.length; i++) {
 			int sum = 0;
@@ -26,6 +26,8 @@ public class ArrayInArrayExample {
 		
 		System.out.println("--------------------");
 		
+//		2. 과목별 평균
+		
 		for(int j=0; j<3; j++) {
 			int sum = 0;
 			for(int i=0; i<5; i++) {
@@ -37,6 +39,17 @@ public class ArrayInArrayExample {
 		
 		System.out.println("--------------------");
 */	
+		int[] sum2 = new int[3];
+		
+		for(int i=0; i<scores.length; i++) {
+			for(int j=0; j<scores[i].length; j++) {
+				sum2[j] += scores[i][j];
+			}
+		}
+		
+		for(int i=0; i<sum2.length; i++) {
+			System.out.println(subject[i]+ " 과목의 평균 점수는 : " +((double)sum2[i]/scores.length));;
+		}
 		
 		// 점수 리버스&트레이드
 		
